9e0ff1eccf04a340ee115dee858cace6403823e8
BISMILAHONLINE <114799967+BISMILAHONLINE@users.noreply.github.com>
2022-10-14 00:12:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6288980870067']
+global.ownernomer = "6288980870067"
+global.premium = ['6288980870067']
+global.packname = 'Siesta - Multi Device'
+global.author = 'Swift Kawai'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
